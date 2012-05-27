d182589ab9674fae7558febc8543326455012016
av <av@fun-box.ru>
2012-05-28 01:01:30 +0400

    remove redundant lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,15 +6,11 @@ ROOT = File.expand_path('../..', __FILE__)
 
 Bundler.require(:default, :test, :development)
 
-SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-
 SimpleCov.start
 
 require 'pulse-meter'
 require 'pulse-meter/visualizer'
 require 'rack/test'
-
-
 
 Dir['spec/support/**/*.rb'].each{|f| require File.join(ROOT, f) }
 Dir['spec/shared_examples/**/*.rb'].each{|f| require File.join(ROOT,f)}
