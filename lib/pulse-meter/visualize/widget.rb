c5c874285e8f11d6fe86fcfe6c229e53de3e9213
av <av@fun-box.ru>
2012-12-01 00:18:22 +0400

    remove the unused copy of a mysteriously duplicated method

--- lib/pulse-meter/visualize/widget.rb
+++ lib/pulse-meter/visualize/widget.rb
@@ -25,22 +25,6 @@ module PulseMeter
 
       protected
 
-      def ensure_sensor_match!
-        intervals = []
-        sensors.each do |s|
-          unless s.type < PulseMeter::Sensor::Timeline
-            raise NotATimelinedSensorInWidget, "sensor `#{s.name}' is not timelined"
-          end
-          intervals << s.interval
-        end
-
-        unless intervals.all?{|i| i == intervals.first}
-          interval_notice = sensors.map{|s| "#{s.name}: #{s.interval}"}.join(', ')
-          raise DifferentSensorIntervalsInWidget, "Sensors with different intervals in a single widget: #{interval_notice}"
-        end
-      end
-
-
       def gauge_series_data
         ensure_gauge_indicators!
         sensors.map do |s|
